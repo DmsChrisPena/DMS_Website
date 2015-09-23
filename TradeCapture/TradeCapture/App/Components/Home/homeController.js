47e90dcc633be61b3dc237883cdfe595fc8b62ad
dmschrispena <cpena@dmshouston.com>
2015-09-23 02:23:44 -0500

    Sending some info

---
+++ TradeCapture/TradeCapture/App/Components/Home/homeController.js
@@ -0,0 +1,19 @@
+﻿(function () {
+    angular.module("TradeCaptureApp").controller("HomeController", ["homeService", HomeController]);
+
+    function HomeController(homeService) {
+        var vm = this;
+        vm.getMarketData = getMarketData;
+        vm.getTradeData = getTradeData;
+
+        function getTradeData() {
+            vm.trades = homeService.getTradeData();
+        }
+        vm.getTradeData();
+
+        function getMarketData() {
+            vm.marketData = homeService.getMarketData();
+        }
+    }
+
+})();
---
+++ TradeCapture/TradeCapture/App/Components/Home/homeService.js
@@ -0,0 +1,35 @@
+﻿(function () {
+    angular.module("TradeCaptureApp").factory("homeService", homeService);
+
+    function homeService() {
+        var service = {};
+
+        service.getMarketData = getMarketData;
+        service.getTradeData = getTradeData;
+
+        function getMarketData() {
+            var x, i, xmlhttp, xmlDoc, table;
+            xmlhttp = new XMLHttpRequest();
+            xmlhttp.open("GET", "/App/MarketData.xml", false);
+            xmlhttp.send();
+            xmlDoc = xmlhttp.responseXML;
+            var json = $.xml2json(xmlDoc);
+            console.log(json);
+            return json;
+        }
+
+        function getTradeData() {
+            var x, i, xmlhttp, xmlDoc, table;
+            xmlhttp = new XMLHttpRequest();
+            xmlhttp.open("GET", "/App/Trade.xml", false);
+            xmlhttp.send();
+            xmlDoc = xmlhttp.responseXML;
+            var json = $.xml2json(xmlDoc);
+            console.log(JSON.stringify(json));
+            console.log(json);
+            return json;
+        }
+
+        return service;
+    }
+})();
