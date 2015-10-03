756d0f8c1de78470b3027c8e54196eb9eaa2069a
heroictugboat <xxmatthewhowardxx@gmail.com>
2015-10-02 19:33:35 -0500

    animations and styling for gaspro and gastrak page

--- DMS/DMS.Web/app/app.module.js
+++ DMS/DMS.Web/app/app.module.js
@@ -1,7 +1,7 @@
 ﻿(function () {
     //Dependencies here
     angular
-        .module("DMSApp", ['ui.router', 'ngMaterial', 'ngMdIcons'])
+        .module("DMSApp", ['ui.router', 'ngMaterial', 'ngMdIcons', 'ngAnimate'])
         .constant('ContactUrls', {
             getTopics: '/api/contact/topics',
             postRequest: '/api/contact/request'
