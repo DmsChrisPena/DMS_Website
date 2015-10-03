1314aefba085da0f3aaff2f5b1c709f3df0ba273
heroictugboat <xxmatthewhowardxx@gmail.com>
2015-10-03 15:06:38 -0500

    took the slider folder

--- DMS/DMS.Web/app/components/solutions/energypro/energyproController.js
+++ DMS/DMS.Web/app/components/solutions/energypro/energyproController.js
@@ -5,7 +5,30 @@
 
     function energyproController() {
         var vm = this;
-        
+
+        function animateIn1 () {
+            
+        }
+
+        function animateOut1() {
+
+        }
+
+        function animateIn2() {
+            dog = [];
+        }
+
+        function animateOut2() {
+
+        }
+
+        function animateIn3() {
+            dog = [];
+        }
+
+        function animateOut3() {
+
+        }
     }
 
 })();
