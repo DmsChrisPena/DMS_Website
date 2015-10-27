7fb7814170b0f2ac07b360576d4237bc23704c1e
dmschrispena <cpena@dmshouston.com>
2015-10-27 13:12:03 -0500

    does this work

--- DMS/DMS.Web/app/app.module.js
+++ DMS/DMS.Web/app/app.module.js
@@ -1,7 +1,7 @@
 ﻿(function () {
     //Dependencies here
     angular
-        .module("DMSApp", ['ui.router', 'ngMaterial', 'ngMdIcons', 'ngAnimate'])
+        .module("DMSApp", ['ui.router', 'ngMaterial', 'ngMdIcons', 'ngAnimate', "ngHamburger"])
         .constant('ContactUrls', {
             getTopics: '/api/contact/topics',
             postRequest: '/api/contact/request'
